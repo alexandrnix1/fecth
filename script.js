f085142f4f8612dd860a38149306424b29d4f8fe
alexandrnix1 <alexandrnix1@gmail.com>
2026-04-14 14:30:31 +0300

    Init commit

--- script.js
+++ script.js
@@ -22,14 +22,14 @@ const button = document.getElementById('fetchButton');
             resultDiv.innerHTML = '';
             
             try {
-                const response = await fetch('https://api.waifu.pics/sfw/neko');
+                const resp = await fetch('https://nekos.best/api/v2/neko');
                 
                 
-                if (!response.ok) {
-                    throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+                if (!resp.ok) {
+                    throw new Error(`HTTP error: ${resp.status} ${resp.statusText}`);
                 }
                 
-                const data = await response.json();
+                const data = await resp.json();
 
                 const imageUrl = data.url;
                 
